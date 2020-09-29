cdbd7d516b832cabe0790f6776252ff9bf26c7a0
Justin Wozniak <woz@anl.gov>
2020-09-29 18:41:09 -0500

    Copy for changes

---
+++ 2020-09-29/testing_swift-t_on_summit/test-2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#BSUB -W 00:05
+#BSUB -nnodes 2
+#BSUB -P med106
+#BSUB -q debug
+
+WORKFLOWS_ROOT=
+PROCS=
+source "/gpfs/alpine/world-shared/med106/weismana/sw/candle/initial/Supervisor/workflows/common/sh/env-summit.sh"
+
+which swift-t
+which turbine
+which stc
+
+echo "------------------------"
+cat a.tic
+echo "------------------------"
+turbine -n 5 a.tic
+
+echo "------------------------"
+cat b.swift
+echo "------------------------"
+stc b.swift
+
+echo "------------------------"
+cat b.tic
+echo "------------------------"
+turbine -n 5 b.tic
